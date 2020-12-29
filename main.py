cb71e9828fcb80eed4cf30b96af6bf465588a212
ZainJaffer <zain@zainjaffer.com>
2020-12-28 16:09:43 -0800

    - Create XLSX file locally - Ping stock APIs - Pull in last 100 days of Apple stock prices

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from openpyxl import Workbook, styles
-import os
+from openpyxl.chart import LineChart,Reference
+from openpyxl.chart.axis import DateAxis
 import requests
 
-#--- Creating sheet and styles ---#
+###------- CREATING SHEET AND STYLES ---------###
 
 wb = Workbook()
 ws = wb.active
@@ -10,18 +11,17 @@ ws.title = "Summary"
 ws['A1'] = 'Summary Page'
 title_style = styles.Font(bold=True, sz=30, underline='single', italic=True)
 ws['A1'].font = title_style
-wb.save('stock_tracker.xlsx')
+apple_tab = wb.create_sheet("Apple Tab")
+apple_tab['A1'] = "Apple raw data"
+apple_tab['A1'].font = title_style
 
-#TODO - Get stock prices
-#TODO - Create a Google sheet version
 
-#------- STOCK API & VARIABLES (TO HASH LATER) ---------#
+###------- API VARIABLES ---------###
 
 STOCK = "AAPL"
 COMPANY_NAME = "Apple"
 
 stock_api_key = 'P80S76BFMQ83FJBH'
-
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 
 stock_param = {
@@ -34,19 +34,71 @@ stock_param = {
 
 response = requests.get(STOCK_ENDPOINT, params=stock_param)
 response.raise_for_status()
-data = response.json()["Time Series (Daily)"]
+stock_data = response.json()["Time Series (Daily)"]
 
-today = list(data.keys())[0]
-yesterday = list(data.keys())[1]
+today = list(stock_data.keys())[0]
+yesterday = list(stock_data.keys())[1]
 
 #------- RUN STOCK REQUESTS ---------#
 
+apple_daily_prices = []
+apple_date = []
+apple_low = []
+apple_high = []
+
+#TODO: Fix bug below to get data into a format ready for excel chart.
+
 def value_check():
-    today_open = float((data[today]['1. open']))
-    today_close = float((data[yesterday]['1. open']))
-    change = round(((today_open - today_close) / today_open)*100,2)
-    print(f"Today's stock price for {COMPANY_NAME} : {today_open}")
-    print(f"Yesterday's price for {COMPANY_NAME}: {today_close}")
-    print(f"Percentage change is {change}%")
+    for i in stock_data.keys():
+        high_price = float((stock_data[i]['2. high']))
+        low_price = float((stock_data[i]['3. low']))
+        apple_date.append(i)
+        apple_low.append(low_price)
+        apple_high.append(high_price)
+        apple_daily_prices.append([i, high_price, low_price]) # converts into list format for dataentry
 
 value_check()
+
+print(apple_daily_prices)
+
+
+#------- CREATING CHART ---------#
+
+rows = [
+    ['Date', 'Low', 'High']]
+
+
+for row in rows:
+    ws.append(row)
+    for i in range(len(apple_daily_prices)):
+        ws.append(apple_daily_prices[i])
+
+
+c1 = LineChart()
+c1.title = "Test Chart"
+c1.style = 13
+c1.y_axis.title = 'Size'
+c1.x_axis.title = 'Test Number'
+
+data = Reference(ws, min_col=2, min_row=1, max_col=4, max_row=7)
+c1.add_data(data, titles_from_data=True)
+
+# Style the lines
+s1 = c1.series[0]
+s1.marker.symbol = "triangle"
+s1.marker.graphicalProperties.solidFill = "FF0000" # Marker filling
+s1.marker.graphicalProperties.line.solidFill = "FF0000" # Marker outline
+
+s1.graphicalProperties.line.noFill = True
+
+s2 = c1.series[1]
+s2.graphicalProperties.line.solidFill = "00AAAA"
+s2.graphicalProperties.line.dashStyle = "sysDot"
+s2.graphicalProperties.line.width = 100050 # width in EMUs
+
+s2 = c1.series[2]
+s2.smooth = True # Make the line smooth
+
+ws.add_chart(c1, "E10")
+
+wb.save('stock_tracker.xlsx')
